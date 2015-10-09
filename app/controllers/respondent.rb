518f7164c14e9ecb5fa2c3a2ec3ecc3e3fcc401a
Angelita Graves <angelita.l.graves@vanderbilt.edu>
2015-10-09 18:49:43 -0400

    fix question display

--- app/controllers/respondent.rb
+++ app/controllers/respondent.rb
@@ -4,7 +4,7 @@ end
 
 get '/surveys/respondent/:survey_id' do
   @survey = Survey.find_by(id: params[:survey_id])
-  @questions = Question.all.where(survey_id: :survey_id)
+  @questions = @survey.questions
   erb :'respondent/survey'
 end
 
@@ -12,7 +12,7 @@ post '/surveys/respondent/:survey_id' do
   # erb params.inspect
   @completion = Completion.new(user: @user, survey: @survey)
   Choice.all.each do |selection|
-    @completion << Selection.new(completion: @completion, choice_id: choice_id)
+    Selection.new(completion: @completion, choice: selection)
   end
 
 end
