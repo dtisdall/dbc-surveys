0d22e9e17691a897a8f6a9567dff421d20095316
Angelita Graves <angelita.l.graves@vanderbilt.edu>
2015-10-09 18:12:04 -0400

    working on controller for creating a completion from the selections- not to be merged

--- app/controllers/respondent.rb
+++ app/controllers/respondent.rb
@@ -9,5 +9,10 @@ get '/surveys/respondent/:survey_id' do
 end
 
 post '/surveys/respondent/:survey_id' do
-  erb params.inspect
+  # erb params.inspect
+  @completion = Completion.new(user: @user, survey: @survey)
+  Choice.all.each do |selection|
+    @completion << Selection.new(completion: @completion, choice_id: choice_id)
+  end
+
 end
