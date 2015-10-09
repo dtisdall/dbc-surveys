2a73a165b76785a0a0cdce927d197a0c7b7bb967
Chanyoung Kim & Peter Rosal <pair+kimchanyoung.r05al@devbootcamp.com>
2015-10-09 15:24:31 -0400

    Add question creation flow

---
+++ app/controllers/question.rb
@@ -0,0 +1,17 @@
+get '/surveys/:id/questions/new' do
+  @question = Question.new
+  if logged_in?
+    erb :"/questions/new", locals: {survey_id: params[:id]}
+  else
+    redirect '/login'
+  end
+end
+
+post '/surveys/:id/questions' do
+  @question = Question.new(params["question"])
+  if @question.save
+    redirect "/questions/#{@question.id}/choices/new"
+  else
+    redirect "surveys/#{params[:id]}/questions/new"
+  end
+end
