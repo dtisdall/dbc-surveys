d7658e838f4d08338abbeca734f697aecd5474de
David Tisdall <david.tisdall@katasan.com>
2015-10-11 23:28:49 -0400

    Improve new survey forms to match application style

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,6 +3,7 @@ class Survey < ActiveRecord::Base
   has_many :completions
   has_many :questions
   has_many :respondents, through: :completions, class_name: 'User'
+  validates :title, presence: true
 
   before_save do
     self.key = SecureRandom.hex(12)
