d585be08ffc1b0c687a51f6d2b85c8bf42c83d65
Angelita Graves <angelita.l.graves@vanderbilt.edu>
2015-10-11 20:43:19 -0400

    attempt to incorporate the ajax for the survey form. Do not merge.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,41 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+$( document ).ready(function() {
+  debugger;
+    console.log( "ready!" );
 });
+
+
+// $(document).ready(function() {
+//   // This is called after the document has loaded in its entirety
+//   // This guarantees that any elements we bind to will exist on the page
+//   // when we try to bind to them
+
+//   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+//   // <% item = 1 %>
+//   // $(button).on("click", function(event){
+//   //    event.preventDefault();
+//   //    console.log("Great, you prevented it!! ^_^");
+
+//   $('.question_button').on("click", function(event){
+//     debugger;
+//     event.preventDefault();
+//     console.log("Great, you prevented it!! ^_^");
+//   })
+// });
+
+// // $('#').on('click', function(){
+// //     alert('naex has been clicked!');
+// // });
+
+// // function showNextQuestion(el) {
+// //     alert (el.className);
+// // }
+//   // $("onClick").on("click", function(event){
+//   //   event.preventDefault();
+//   //   function showNextQuestion(current,next){
+//   // debugger;
+//   // document.getElementById(current).style.display = "none";
+//   // document.getElementById(next).style.display = "on";
+// }
